bb204f9e5ff3586062a40394d273ecf97b44da93
Hans <hans@brouwer.work>
2022-01-11 21:28:05 +0000

    more optimizers!

--- maua/optimizers.py
+++ maua/optimizers.py
@@ -1,11 +1,18 @@
+from copy import deepcopy
+
+import timm.optim as timm_optim
 import torch_optimizer as more_optim
+from ranger import Ranger, RangerQH, RangerVA
+from ranger21 import Ranger21
 from torch import optim
 
 optimizer_choices = {
     "AccSGD": more_optim.AccSGD,
+    "AdaBelief": timm_optim.AdaBelief,
     "AdaBound": more_optim.AdaBound,
     "Adadelta": optim.Adadelta,
     "Adagrad": optim.Adagrad,
+    "Adahessian": timm_optim.Adahessian,
     "Adam": optim.Adam,
     "AdamW": optim.AdamW,
     "Adamax": optim.Adamax,
@@ -14,14 +21,24 @@ optimizer_choices = {
     "AdamP": more_optim.AdamP,
     "AggMo": more_optim.AggMo,
     "DiffGrad": more_optim.DiffGrad,
+    "FusedSGD": timm_optim.optim_factory.FusedSGD,
+    "FusedAdam": timm_optim.optim_factory.FusedAdam,
+    "FusedLAMB": timm_optim.optim_factory.FusedLAMB,
+    "FusedNovoGrad": timm_optim.optim_factory.FusedNovoGrad,
     "Lamb": more_optim.Lamb,
     "NAdam": optim.NAdam,
-    "NovoGrad": more_optim.NovoGrad,
+    "NovoGrad": timm_optim.NovoGrad,
+    "NvNovoGrad": timm_optim.NvNovoGrad,
     "PID": more_optim.PID,
     "QHAdam": more_optim.QHAdam,
     "QHM": more_optim.QHM,
     "RAdam": more_optim.RAdam,
+    "Ranger": Ranger,
+    "RangerQH": RangerQH,
+    "RangerVA": RangerVA,
+    "Ranger21": Ranger21,
     "RMSprop": optim.RMSprop,
+    "RMSpropTF": timm_optim.RMSpropTF,
     "SGD": optim.SGD,
     "SGDP": more_optim.SGDP,
     "SGDW": more_optim.SGDW,
@@ -43,6 +60,21 @@ def load_optimizer(name, lr, kwargs, n_iters, params):
 
     if name in optimizer_choices:
         return optimizer_choices[name](params, lr=lr, **kwargs), n_iters
+
+    if "Lookahead-" in name:
+        if "lookahead_alpha" in kwargs:
+            alpha = deepcopy(kwargs["lookahead_alpha"])
+            del kwargs["lookahead_alpha"]
+        else:
+            alpha = 0.5
+
+        if "lookahead_k" in kwargs:
+            k = deepcopy(kwargs["lookahead_k"])
+            del kwargs["lookahead_k"]
+        else:
+            k = 0.5
+
+        return timm_optim.Lookahead(load_optimizer(name.split("-")[-1], lr, kwargs, n_iters, params), alpha, k)
 
     raise Exception(
         f"Optimizer {name} not recognized! Choices are: {['LBFGS', 'LBFGS20'] + list(optimizer_choices.keys())}"
